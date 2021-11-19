ba453cb2705b06ceb6b15fa59e1b8a96079efbad
Zenlian <zeninglian@gmail.com>
2021-11-20 01:57:53 +0800

    ts-rainbow

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -9,9 +9,17 @@ editor["nvim-treesitter/nvim-treesitter-textobjects"] = {
     opt = true,
     after = "nvim-treesitter"
 }
+--editor["nvim-treesitter/nvim-treesitter-refactor"] = {
+--    opt = true,
+--    after = "nvim-treesitter"
+--}
 editor["romgrk/nvim-treesitter-context"] = {
     opt = true,
     after = "nvim-treesitter"
+}
+editor["p00f/nvim-ts-rainbow"] = {
+    opt = true,
+    after = "nvim-treesitter",
 }
 
 return editor
