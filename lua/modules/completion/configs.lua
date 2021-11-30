a0e3d3575870da380303a15f8dff0c427c7f2ff8
Zenlian <zeninglian@gmail.com>
2021-11-30 21:44:21 +0800

    nvim-cmp: show kind name

--- lua/modules/completion/configs.lua
+++ lua/modules/completion/configs.lua
@@ -117,8 +117,7 @@ configs["hrsh7th/nvim-cmp"] = function()
         formatting = {
             format = function(entry, vim_item)
                 -- Kind icons
-                -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-                vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
+                vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
                 -- Source
                 vim_item.menu = ({
                     nvim_lsp = "[LSP]",
