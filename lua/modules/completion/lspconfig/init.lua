0bba7b6b205fb7143c7945677fb101db2f772525
Zenlian <zeninglian@gmail.com>
2022-05-13 00:49:23 +0800

    update lsp config

--- lua/modules/completion/lspconfig/init.lua
+++ lua/modules/completion/lspconfig/init.lua
@@ -1,3 +1,5 @@
+local servers = {"sumneko_lua", "ccls"}
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -19,32 +21,36 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
     buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
     buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', '<space>lD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<space>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    buf_set_keymap('n', '<space>le', '<cmd>lua vim.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<space>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 local setup = function()
-    local lsp_installer = require("nvim-lsp-installer")
-    lsp_installer.on_server_ready(function(server)
+    require("nvim-lsp-installer").setup{
+        ensure_installed = servers
+    }
+
+    local lspconfig = require('lspconfig')
+    for _, server in ipairs(servers) do
         local opts = {
             on_attach = on_attach,
             flags = {
                 debounce_text_changes = 150
             }
         }
-        local ok, custom_opts = pcall(require, 'modules.completion.lspconfig.'..server.name)
+        local ok, custom_opts = pcall(require, 'modules.completion.lspconfig.'..server)
         if ok then
             opts = vim.tbl_deep_extend('force', opts, custom_opts)
         end
-        server:setup(opts)
-    end)
+        lspconfig[server].setup(opts)
+    end
 end
 
 return {
--- lua/modules/completion/lspserver/init.lua
+++ lua/modules/completion/lspserver/init.lua
@@ -2,7 +2,7 @@ local M = {}
 local custom_servers = {"pyls"}
 
 M.setup = function()
-    for index, server in ipairs(custom_servers) do
+    for _, server in ipairs(custom_servers) do
         require("modules.completion.lspserver."..server)
     end
 end
