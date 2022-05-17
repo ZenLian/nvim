cc0a87b1ccf60de5e90ff215023f7ee0f94ca34e
Zenlian <zeninglian@gmail.com>
2022-05-17 18:58:39 +0800

    disable some builtin pluginss

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,8 +1,31 @@
-vim.g.mapleader = " "
 
 -- speedup
 require('impatient').enable_profile()
+local disabled_built_plugins = {
+    "netrw",
+    "gzip",
+    "zip",
+    "netrwPlugin",
+    "netrwSettings",
+    "tar",
+    "tarPlugin",
+    "netrwFileHandlers",
+    "zipPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "spellfile_plugin",
+    "matchit",
+}
+for _, plugin in ipairs(disabled_built_plugins) do
+    vim.g["loaded_" .. plugin] = 1
+end
 
+
+vim.g.mapleader = " "
 require('core.options')
 require('core.autocmd')
 -- require('core.keymaps')
