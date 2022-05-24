2da2871c735b7afda007050c79f19c0dd14e2ee9
Zenlian <zeninglian@gmail.com>
2022-05-25 02:05:35 +0800

    add github lint workflows

--- lua/plugins/tools/nvim_tree.lua
+++ lua/plugins/tools/nvim_tree.lua
@@ -1,20 +1,20 @@
-require("nvim-tree").setup({
-    update_focused_file = {
-        enable = true,
-        -- update_cwd = true
+require('nvim-tree').setup({
+  update_focused_file = {
+    enable = true,
+    -- update_cwd = true
+  },
+  filters = {
+    custom = { '^\\.git$' },
+  },
+  view = {
+    width = 30,
+    mappings = {
+      list = {
+        { key = 'l', action = 'edit' },
+        { key = 'h', action = 'close_node' },
+      },
     },
-    filters = {
-        custom = { "^\\.git" },
-    },
-    view = {
-        width = 30,
-        mappings = {
-            list = {
-                { key = "l", action = "edit" },
-                { key = "h", action = "close_node" },
-            }
-        }
-    }
+  },
 })
 
 -- https://github.com/romgrk/barbar.nvim#integration-with-filetree-plugins
@@ -22,9 +22,9 @@ local nvim_tree_events = require('nvim-tree.events')
 local bufferline_state = require('bufferline.state')
 
 nvim_tree_events.on_tree_open(function()
-    bufferline_state.set_offset(30, "File Tree")
+  bufferline_state.set_offset(30, 'File Tree')
 end)
 
 nvim_tree_events.on_tree_close(function()
-    bufferline_state.set_offset(0)
+  bufferline_state.set_offset(0)
 end)
