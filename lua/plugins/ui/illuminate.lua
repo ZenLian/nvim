76a98bb30d421f388400f726c682df131b7bd4e8
Zenlian <zeninglian@gmail.com>
2023-10-20 17:13:24 +0800

    illuminate: disable for large file

--- lua/plugins/ui/illuminate.lua
+++ lua/plugins/ui/illuminate.lua
@@ -42,6 +42,15 @@ local spec = {
     modes_allowlist = { 'n' },
     -- under_cursor: whether or not to illuminate under the cursor
     under_cursor = true,
+    -- large_file_cutoff: number of lines at which to use large_file_config
+    -- The `under_cursor` option is disabled when this cutoff is hit
+    large_file_cutoff = 5000,
+    -- large_file_config: config to use for large files (based on large_file_cutoff).
+    -- Supports the same keys passed to .configure
+    -- If nil, vim-illuminate will be disabled for large files.
+    large_file_overrides = nil,
+    -- min_count_to_highlight: minimum number of matches required to perform highlighting
+    min_count_to_highlight = 1,
   },
   config = function(_, opts)
     require('illuminate').configure(opts)
